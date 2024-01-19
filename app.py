2184edbd2a739d0c92746d2be968c595418ad8b2
AArias <abdiel.arias@uao.edu.co>
2024-01-19 16:15:13 -0500

    visible app

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def index():
     return render_template('index.html', image_path=result_image_path, pentagon_coords=pentagon_coords)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
